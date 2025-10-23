7d5b090f1eaff3f83d781a96c9c1808687ec8b87
JohnMHoyt24 <67488201+JohnMHoyt24@users.noreply.github.com>
2025-10-23 14:03:58 +0000

    Imported 3 new components: SignupForm, FreeTrial, and IntroMessage

--- signup-form/src/App.tsx
+++ signup-form/src/App.tsx
@@ -1,14 +1,18 @@
 import './App.css'
-import SignupForm from './signup'
+import IntroMessage from './components/intro-message';
+import FreeTrial from './components/freetrial';
+import SignupForm from './components/signup';
 
 function App() {
 
 
   return (
-    <>
+    <div>
+      <IntroMessage />
+      <FreeTrial />
       <SignupForm />
-    </>
+    </div>  
   )
 }
 
-export default App
+export default App;
